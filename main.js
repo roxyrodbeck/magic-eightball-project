0d4e0331bbada26e0da657d68f1364a7e2709387
Roxana Rodriguez-Becker <126708010+roxyrodbeck@users.noreply.github.com>
2023-11-10 14:08:11 -0600

    Update main.js
    corrections based on dev30 answer video

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let userName = "Roxy";
+const userName = "Roxy";
 if (userName) {
     console.log(`Hello, ${userName}!`)
 } else {
     console.log("Hello!")
 }
 
-let userQuestion = "";
+let userQuestion = "will I get a coding job by September?";
 if (userQuestion) {
     console.log(`${userName} wonders ${userQuestion}`)
 }
@@ -14,20 +14,23 @@ let randomNumber = Math.floor(Math.random() * 8);
 
 
 let eightBall = "";
+
 if (randomNumber === 0) {
-    console.log('You go, Glen Coco!')
+    eightBall= 'You go, Glen Coco!';
 } else if (randomNumber === 1) {
-    console.log(`Well, ${userName}, it is decidely so.`)
+    eightBall = `Well, ${userName}, it is decidely so.`;
 } else if (randomNumber === 2) {
-    console.log("Reply hazy, let me grab my glasses. Try again in a minute.")
+    eightBall = "Reply hazy, let me grab my glasses. Try again in a minute.";
 } else if (randomNumber === 3) {
-    console.log(`Oops, come back later, ${userName}!`)
+    eightBall = `Oops, come back later, ${userName}!`;
 } else if (randomNumber === 4) {
-    console.log("Ya, don't count on it.")
+    eightBall = "Ya, don't count on it.";
 } else if (randomNumber === 5) {
-    console.log(`Sorry, ${userName}, but my sources say no.`)
+    eightBall = `Sorry, ${userName}, but my sources say no.`;
 } else if (randomNumber === 6) {
-    console.log("WOMP WOMP. Outlook not so good.")
+    eightBall = "WOMP WOMP. Outlook not so good.";
 } else if (randomNumber === 7) {
-    console.log(`Oooooo see that sign over there? It's pointing to YES, ${userName}!`)
+    eightBall = `Oooooo see that sign over there? It's pointing to YES, ${userName}!`;
 }
+
+console.log(`Your answer is ${eightBall}`);
